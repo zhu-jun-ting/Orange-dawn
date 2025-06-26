d54ca3f39c77361b258c30615de9b096dbefd936
David Zhu <david007112@outlook.com>
2025-06-26 19:54:17 +0800

    gun bullet can bounce

--- Assets/Scripts/Weapon/GunBullet.cs
+++ Assets/Scripts/Weapon/GunBullet.cs
@@ -10,9 +10,15 @@ public class GunBullet : MonoBehaviour
     public GameObject explosionPrefab;
     new private Rigidbody2D rigidbody;
     public List<string> trigger_tags; 
-    public float hit_back = 0.01f;
+    public List<string> bounce_tags; 
+    public float bounce_randomness = 10f; // how much angle the bullet can randomly bounce off walls, 0 means no randomness
+    public float inertia = 0f; // how much inertia the bullet has, 0 means no inertia
+
+    public float hit_back = 5f;
     public Gun gun; // the gun that fired this bullet, used for source of damage and other effects
     // all tags included gameobjects will trigger this bullet to hurt
+    [Tooltip("Layers that the bullet will bounce off like pinball")]
+    public LayerMask bounceLayers;
 
     void Awake()
     {
@@ -52,28 +58,74 @@ public class GunBullet : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (trigger_tags.Contains(other.tag))
+        // if (trigger_tags.Contains(other.tag))
+        // {
+        //     if (other != null)
+        //     {
+        //         // Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        //         // other.gameObject.GetComponent<IBuffable>().TakeDamage(att, GameEvents.DamageType.Normal, hit_back, owner.gameObject, gun);
+        //         GameObject exp = ObjectPool.Instance.GetObject(explosionPrefab);
+        //         exp.transform.position = transform.position;
+
+        //         PawnMaster pawnMaster = other.gameObject.GetComponent<PawnMaster>();
+        //         if (pawnMaster != null) 
+        //         {
+        //             if (owner == PlayerController.instance.gameObject && GameEvents.OnModifyDamage != null) 
+        //             {
+        //                 att = GameEvents.OnModifyDamage(att); 
+        //             } 
+        //             GameEvents.instance.HitPawn(att, pawnMaster, gameObject, GameEvents.DamageType.Normal, pawnMaster.gameObject.transform, hit_back, gun);
+        //         } 
+        //     }
+        // }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the collision is with a trigger tag to deal damage
+        if (trigger_tags.Contains(collision.collider.tag))
         {
-            if (other != null)
+            if (collision != null)
             {
                 // Instantiate(explosionPrefab, transform.position, Quaternion.identity);
                 // other.gameObject.GetComponent<IBuffable>().TakeDamage(att, GameEvents.DamageType.Normal, hit_back, owner.gameObject, gun);
                 GameObject exp = ObjectPool.Instance.GetObject(explosionPrefab);
                 exp.transform.position = transform.position;
 
-                PawnMaster pawnMaster = other.gameObject.GetComponent<PawnMaster>();
+                PawnMaster pawnMaster = collision.gameObject.GetComponent<PawnMaster>();
                 if (pawnMaster != null) 
                 {
                     if (owner == PlayerController.instance.gameObject && GameEvents.OnModifyDamage != null) 
                     {
                         att = GameEvents.OnModifyDamage(att); 
                     } 
-                    GameEvents.instance.HitPawn(att, pawnMaster, gameObject, GameEvents.DamageType.Normal, pawnMaster.gameObject.transform, 0f, gun);
+                    GameEvents.instance.HitPawn(att, pawnMaster, gameObject, GameEvents.DamageType.Normal, pawnMaster.gameObject.transform, hit_back, gun);
                 } 
             }
-            // ObjectPool.Instance.PushObject(gameObject);
-            Destroy(gameObject);
+        }
+
+        // Check for bounce by tag or by layer
+        bool shouldBounce = bounce_tags.Contains(collision.collider.tag) || ((bounceLayers.value & (1 << collision.collider.gameObject.layer)) != 0);
+        if (shouldBounce)
+        {
+            Vector2 normal = collision.contacts[0].normal;
+            Vector2 incoming = rigidbody.linearVelocity;
+            float speed = incoming.magnitude;
+            float angle = Random.Range(-bounce_randomness, bounce_randomness);
+            Vector2 reflected = Vector2.Reflect(incoming, normal);
+            reflected = Quaternion.Euler(0, 0, angle) * reflected;
+            if (inertia > 0f)
+                speed *= Mathf.Clamp01(1f - inertia * Time.fixedDeltaTime);
+            rigidbody.linearVelocity = reflected.normalized * speed;
         }
     }
-    
+
+    void FixedUpdate()
+    {
+        // Apply inertia damping every physics step
+        if (inertia > 0f && rigidbody.linearVelocity.magnitude > 0.01f)
+        {
+            rigidbody.linearVelocity *= Mathf.Clamp01(1f - inertia * Time.fixedDeltaTime);
+        }
+    }
 }
